311cf4cd136a57ba9244cd905420451eccd913cb
Amir01010 <92575031+Amir01010@users.noreply.github.com>
2021-12-03 12:19:41 +0100

    first commit

---
+++ array.js
@@ -0,0 +1,29 @@
+
+
+// let movi =[
+//     ['movi 1,jrr,1980'],
+//     ['movi 2 ,jpr,2021'],
+//     ['movi 3,jjt ,2010'],
+//     ];
+    
+//     console.log(person)
+    
+//     for (let i = 0;  i <personen lenbth;i++)
+//     {console.log(person[i]);
+//     };
+
+
+// switch(movi)
+// case{ '1':
+// console.log('movi 1');
+// break;
+
+
+// case{ '2':
+// console.log('movi 2');
+// break;
+
+
+case{ '3':
+console.log('movi 3');
+
